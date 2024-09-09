a3257aa46eb4db532195852217b824e98b103e58
Dikshant Khatri <khatridikshant@outlook.com>
2024-09-09 12:16:37 +0545

    get_db connected with routes.py

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args ={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args ={"check_same_thread": False},pool_recycle=300)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
--- user/routes.py
+++ user/routes.py
@@ -1,6 +1,7 @@
 import fastapi.routing as router
 from fastapi import status,Depends
 from sqlalchemy.orm import Session
+from db import get_db
 
 _router = router.APIRouter(prefix="/users", tags=["User"],responses={404: {"description" : "Not Found"}})
 
