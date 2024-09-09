56531c8ca10aa0bfb555fe54d9d20db3e6cbb7a6
Dikshant Khatri <khatridikshant@outlook.com>
2024-09-09 12:13:48 +0545

    get_db function created

--- user/routes.py
+++ user/routes.py
@@ -1,2 +1,8 @@
 import fastapi.routing as router
-router = router.APIRouter()
+from fastapi import status,Depends
+from sqlalchemy.orm import Session
+
+_router = router.APIRouter(prefix="/users", tags=["User"],responses={404: {"description" : "Not Found"}})
+
+@_router.post("",status_code=status.HTTP_201_CREATED)
+async def create_user(data: CreateUserRequest, db:Session = Depends(get_db)):
