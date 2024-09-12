5b93431f8a3ece8ad8920135747a31f69bef9420
Dikshant Khatri <khatridikshant@outlook.com>
2024-09-12 20:30:01 +0545

    Working On OAuth2Scheme

--- security.py
+++ security.py
@@ -1,6 +1,8 @@
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenURL="/auth/token")
 
 
 def get_password_hash(password):
