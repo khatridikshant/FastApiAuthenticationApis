0b91a3330947bcca781ac1d1cbd799dabc3d5931
Dikshant Khatri <khatridikshant@outlook.com>
2024-09-12 20:36:37 +0545

    Authentication Setup

---
+++ auth/route.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+from fastapi import status
+from fastapi.security import OAuth2AuthorizationCodeBearer,OAuth2PasswordRequestForm
+
+
+router = APIRouter(prefix="/auth",tags=["Auth"],responses={404: {"description": "Not Found"}})
+
+@router.post("/token",status_code=status.HTTP_200_OK)
+async def authenticate_user(data):
