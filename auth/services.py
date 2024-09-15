10802be8f1a9ffee3450ae460a8af4ceb96e6e5d
Dikshant Khatri <khatridikshant@outlook.com>
2024-09-15 12:00:15 +0545

    Login Access Authentication

--- auth/services.py
+++ auth/services.py
@@ -1,3 +1,20 @@
+import fastapi
+from fastapi import Depends, HTTPException
+import sqlalchemy
+import sqlalchemy.orm
+from db import  get_db
+from user.models import UserModel
+from fastapi.security import OAuth2PasswordRequestForm
+import security
 
-async def get_token(data, db):
-    pass
+
+
+async def get_token(data: OAuth2PasswordRequestForm, db: sqlalchemy.orm.Session ):
+    user = db.query(UserModel).filter(UserModel.email == data.username).first()
+    if user is None:
+        raise HTTPException(status_code=400,detail="Not Registerd Email")
+
+
+    if not security.verify_password(data.password,user.password):
+         raise HTTPException(status_code=400,detail="Invalid Login Credentials")
+
