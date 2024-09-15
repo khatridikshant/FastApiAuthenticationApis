f4028ff178398ef95355622d286a74c01064610f
Dikshant Khatri <khatridikshant@outlook.com>
2024-09-15 12:14:36 +0545

    Installed pyjwt for tokens

--- auth/services.py
+++ auth/services.py
@@ -6,6 +6,7 @@ from db import  get_db
 from user.models import UserModel
 from fastapi.security import OAuth2PasswordRequestForm
 import security
+import datetime
 
 
 
@@ -17,4 +18,12 @@ async def get_token(data: OAuth2PasswordRequestForm, db: sqlalchemy.orm.Session 
 
     if not security.verify_password(data.password,user.password):
          raise HTTPException(status_code=400,detail="Invalid Login Credentials")
+    
+    return '' # Return Access and Refresh Tokens
+
+
+
+def get_user_token(user: UserModel, refresh_token = None):
+    payload = {"id": user.id, "time": str(datetime.datetime.now())}
+
 
